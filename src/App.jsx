9d74a7f3601b1a1c8f9ae339bd13ef7cd55173c8
Jéssyca <jessycarozeene@gmail.com>
2020-02-04 17:48:37 -0300

    💫Adding css background and dependencies

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './scss/App.scss';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <div class="stars"></div>
+        <div class="twinkling"></div>
+        <div class="clouds"></div>
+        <h1>alguma coisa</h1>
+    </div>
+      );
+  }
+}
+
+export default App;
