9835752ad86403a92b07c4809eea6da1e7321343
Jéssyca <jessycarozeene@gmail.com>
2020-02-05 10:49:08 -0300

    Update logo and addim strormtrooper svg🤭

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
-import Image from './components/Logo';
+import ImageLogo from './components/ImageLogo';
+import ImageBackg from './components/ImageBackg';
+
+import './scss/ImageBackg.scss';
+
 import './scss/App.scss';
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        <div class="stars"></div>
-        <div class="twinkling"></div>
-        <div class="clouds"></div>
-        <div className="container">
-          <Logo/>
-        </div>
+          <div className="container">
+                <ImageLogo/>
+
+                <ImageBackg/>
+    </div>
+                
     </div>
       );
   }
+
 }
 
 export default App;
--- src/components/ImageLogo.jsx
+++ src/components/ImageLogo.jsx
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import Logo from '../assets/logo.png';
+import Logo from '../assets/Star-Wars.png';
 
 class ImageLogo extends Component{
 	render(){
 		return(
-			<img className="app__logo big" src={Logo} alt="Welcome the game"/>
+			<img className="app__logo big" src={Logo} alt="Welcome the game" />
 
 		)
 	}
