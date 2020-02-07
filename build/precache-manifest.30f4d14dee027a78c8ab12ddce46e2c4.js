98ffff122835e07f9b2f114550f7eb91b91d7515
Jéssyca <jessycarozeene@gmail.com>
2020-02-06 22:02:13 -0300

    Build files 🤷‍♀️

---
+++ build/precache-manifest.30f4d14dee027a78c8ab12ddce46e2c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "feb8dd0181767c9e480be118e6373af8",
+    "url": "/static/media/Star-Wars.feb8dd01.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2228f2c728ae70250be5",
+    "url": "/static/js/main.b703105a.chunk.js"
+  },
+  {
+    "revision": "055492277c23dd80cf68",
+    "url": "/static/js/2.e1b65372.chunk.js"
+  },
+  {
+    "revision": "2228f2c728ae70250be5",
+    "url": "/static/css/main.80523299.chunk.css"
+  },
+  {
+    "revision": "b16ee99844336a3354d05cc8d73f57b4",
+    "url": "/index.html"
+  }
+];
